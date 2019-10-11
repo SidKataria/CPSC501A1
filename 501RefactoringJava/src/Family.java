5c4d4de5c4d36cf678d7fecab971f3dbf40b99b7
Siddharth Kataria <32744185+SidKataria@users.noreply.github.com>
2019-10-11 14:39:59 -0600

    v1.0
    Rename refactoring done on variables in all classes and tested for original output.

--- 501RefactoringJava/src/Family.java
+++ 501RefactoringJava/src/Family.java
@@ -1,24 +1,27 @@
-
+/***
+ * Name: Siddharth Kataria
+ * UCID: 30000880
+ */
 public class Family {
-	public int totalAmount;
+	//public int totalAmount;
 
-    public double[] makeFamily (int age, double base, double exp, double exp2, double exp3, int age2, double base2, double expa, double expb, double expc, double tfsa ) 
+    public double[] makeFamily (int momAge, double momInc, double expenditureM1, double expenditureM2, double expenditureM3, int dadAge, double dadInc, double expenditureD1, double expenditureD2, double expenditureD3, double saving ) 
     	{
-        Mother moms = new Mother(age, base);
-        moms.exp = exp;
-        moms.exp2 = exp2;
-        moms.exp3 = exp3;
+        Mother moms = new Mother(momAge, momInc);
+        moms.expenditure1 = expenditureM1;
+        moms.expenditure2 = expenditureM2;
+        moms.expenditure3 = expenditureM3;
 
-        Father pops = new Father(age2, base2);  
-        pops.exp = expa;
-        pops.exp2 = expb;
-        pops.exp3 = expc;
-        pops.tfsa = tfsa;
+        Father pops = new Father(dadAge, dadInc);  
+        pops.expenditure1 = expenditureD1;
+        pops.expenditure2 = expenditureD2;
+        pops.expenditure3 = expenditureD3;
+        pops.saving = saving;
 
         double[] values = new double[3];
         
         //Base household income
-        values[0] = moms.base + pops.base;
+        values[0] = moms.income + pops.income;
         
         //After expenses
         values[1] = moms.AmountReturned() + pops.AmountReturned();
@@ -27,15 +30,15 @@ public class Family {
         double dadsave = 0.;
 
         //Savings
-        if (age < 25) momsave = 0.05;
-        if (age >= 25 && age < 35 ) momsave = 0.08;
-        if (age >= 35 && age < 61 ) momsave = 0.15;
+        if (momAge < 25) momsave = 0.05;
+        if (momAge >= 25 && momAge < 35 ) momsave = 0.08;
+        if (momAge >= 35 && momAge < 61 ) momsave = 0.15;
 
-        if (age2 < 25) momsave = 0.05;
-        if (age2 >= 25 && age2 < 35 ) momsave = 0.08;
-        if (age2 >= 35 && age2 < 61 ) momsave = 0.15;
+        if (dadAge < 25) dadsave = 0.05;
+        if (dadAge >= 25 && dadAge < 35 ) dadsave = 0.08;
+        if (dadAge >= 35 && dadAge < 61 ) dadsave = 0.15;
 
-        values[2] = moms.base * momsave + dadsave * pops.base;
+        values[2] = moms.income * momsave + dadsave * pops.income;
 
         return values;
     }  
