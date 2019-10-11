a544082e787f07b2c6db1828cf6497044dad85a4
Siddharth Kataria <32744185+SidKataria@users.noreply.github.com>
2019-10-11 16:04:30 -0600

    v2.0
    Created a Parent abstract class to extract variables form Mother and Father java class. Instantiated new Parameters for better input and output of methods. Some renaming was also done for variables and method like amountReturned() became returnedFinalAmount().

--- 501RefactoringJava/src/Father.java
+++ 501RefactoringJava/src/Father.java
@@ -2,22 +2,15 @@
  * Name: Siddharth Kataria
  * UCID: 30000880
  */
-public class Father {
-    public int age;
-    public double income;
-    public double amount;
-    public double expenditure1;
-    public double expenditure2; 
-    public double expenditure3;
-    public double saving;
+public class Father extends Parent {
 
-    public Father (int age, double income ) {
-        this.age = age;
-        this.income = income;
+
+    public Father (double [] stats) {
+        super(stats, "dad");
     }
 
-    public double AmountReturned() {
-        amount = income + age + (age * 0.12) - expenditure1 - expenditure2 - expenditure3 - (income * saving);
-        return amount;
+    public double returnFinalIncome() {
+        finalIncome = income + age + (age * 0.12) - expenditure1 - expenditure2 - expenditure3 - (income * savingsPercent);
+        return finalIncome;
     }
 }
